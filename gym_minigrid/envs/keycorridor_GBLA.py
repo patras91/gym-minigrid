58988c35f222deb1db93758f7d07577970bcacaa
Mark Cavolowsky <mark.cavolowsky@navy.mil>
2021-01-13 22:24:36 -0500

    Initial implementation of matrix environment definition.

--- gym_minigrid/envs/keycorridor_GBLA.py
+++ gym_minigrid/envs/keycorridor_GBLA.py
@@ -1,6 +1,37 @@
-from gym_minigrid.roomgrid import RoomGrid
+from gym_minigrid.roomgrid import RoomGrid, Room
 from gym_minigrid.register import register
 from enum import IntEnum
+
+from ..minigrid import *
+
+class RoomGBLA(Room):
+    def __init__(
+        self,
+        top,
+        size
+    ):
+        # Top-left corner and size (tuples)
+        self.top = top
+        self.size = size
+
+        # List of door objects and door positions
+        # Order of the doors is right, down, left, up
+
+        self.doors = [None] * 4
+        self.door_pos = [None] * 4
+
+
+        # List of rooms adjacent to this one
+        # Order of the neighbors is right, down, left, up
+        self.neighbors = [None] * 4
+
+        # Indicates if this room is behind a locked door
+        self.locked = False
+
+        # List of objects contained
+        self.objs = []
+
+
 
 class KeyCorridorGBLA(RoomGrid):
     """
@@ -55,8 +86,8 @@ class KeyCorridorGBLA(RoomGrid):
 
         super().__init__(
             room_size=taskD.roomSize,
-            num_rows=4,
-            num_cols=6,
+            num_rows=taskD.envD.shape[0],
+            num_cols=taskD.envD.shape[1],
             max_steps=30*taskD.roomSize**2, # may need to be updated
         )
 
@@ -70,7 +101,102 @@ class KeyCorridorGBLA(RoomGrid):
         else:
             self.remove_wall(l[1], l[0], 1)
 
+    def parseDescriptor(self, roomDesc):
+        roomType = (roomDesc & int('0000000011', 2))
+        doorNorth = (roomDesc & int('0000001100', 2))/4
+        doorEast = (roomDesc & int('0000110000', 2))/16
+        doorSouth = (roomDesc & int('0011000000', 2))/64
+        doorWest = (roomDesc & int('1100000000', 2))/256
+
+        return roomType, (doorNorth, doorEast, doorSouth, doorWest)
+
+
+
     def _gen_grid(self, width, height):
+        # Create the grid
+
+
+        self.grid = Grid(width, height)
+        self.grid.wall_rect(0, 0, width, height)
+
+        self.room_grid = []
+
+        # For each row of rooms
+        for j in range(0, self.num_rows):
+            row = []
+
+            # For each column of rooms
+            for i in range(0, self.num_cols):
+                roomType, doorLocs = self.parseDescriptor(self.taskD.envD[j, i])
+
+                room = RoomGBLA(
+                    (i * (self.room_size-1), j * (self.room_size-1)),
+                    (self.room_size, self.room_size)
+                )
+                row.append(room)
+
+                # Generate the walls for this room
+                if roomType > 0:
+                    self.grid.wall_rect(*room.top, *room.size)
+
+            self.room_grid.append(row)
+
+        # The agent starts in the middle, facing right
+        self.agent_pos = (
+            (self.num_cols // 2) * (self.room_size-1) + (self.room_size // 2),
+            (self.num_rows // 2) * (self.room_size-1) + (self.room_size // 2)
+        )
+        self.agent_dir = 0
+
+        # For each row of rooms
+        for j in range(0, self.num_rows):
+            # For each column of rooms
+            for i in range(0, self.num_cols):
+                roomType, (doorNorth, doorEast, doorSouth, doorWest) = \
+                                    self.parseDescriptor(self.taskD.envD[j, i])
+                room = self.room_grid[j][i]
+
+                x_l, y_l = (room.top[0] + 1, room.top[1] + 1)
+                x_m, y_m = (room.top[0] + room.size[0] - 1, room.top[1] + room.size[1] - 1)
+
+                # Door positions, order is right, down, left, up
+                if doorEast:
+                    room.neighbors[0] = self.room_grid[j][i+1]
+                    room.door_pos[0] = (x_m, self._rand_int(y_l, y_m))
+
+                    self.set_door(doorEast, (i, j), room.door_pos[0])
+                if doorSouth:
+                    room.neighbors[1] = self.room_grid[j+1][i]
+                    room.door_pos[1] = (self._rand_int(x_l, x_m), y_m)
+
+                    self.set_door(doorSouth, (i, j), room.door_pos[1])
+                if doorWest:
+                    room.neighbors[2] = self.room_grid[j][i-1]
+                    room.door_pos[2] = (x_l-1, self._rand_int(y_l, y_m))
+
+                    self.set_door(doorWest, (i,j), room.door_pos[2])
+                if doorNorth:
+                    room.neighbors[3] = self.room_grid[j-1][i]
+                    room.door_pos[3] = (self._rand_int(x_l, x_m), y_l-1)
+
+                    self.set_door(doorNorth, (i,j), room.door_pos[3])
+
+
+
+
+    def set_door(self, doorType, roomLoc, doorLoc):
+        if doorType == 0:
+            pass
+        elif doorType == 1:
+            self.grid.set(*doorLoc, None)
+        elif doorType == 2:
+            self.add_door(*roomLoc, locked=False)
+        elif doorType == 3:
+            doorColor = self._rand_color()
+            self.add_door(*roomLoc, locked=True, color=doorColor)
+            self.add_object(self._rand_int(0, self.num_cols), self._rand_int(0, self.num_rows), 'key', doorColor)
+
+    def _gen_grid_old(self, width, height):
         super()._gen_grid(width, height)
 
         # Connect the outside rooms into a hallway by removing the walls
